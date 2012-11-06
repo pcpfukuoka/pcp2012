0ef09d4e9c6cee80aeca374de8fcda34204afcf5
sakatayoshiki <o_hara1045104@mail.goo.ne.jp>
2012-11-06 14:04:39 +0900

    よくわからない
    新規追加

---
+++ kokura.php
@@ -0,0 +1,40 @@
+<html>
+	<head>
+		<meta http-equiv="Content-type" content="text/html; charset=UTF-8">
+		<meta http-equiv="Content-Style-Type" content="text/css">
+		<link rel="stylesheet" type="text/css" href="./css/button.css" />
+		<title>テストウィンドウ</title>
+	</head>
+
+	<body>
+	<form action="" method="POST">
+		<table class="button1">
+			<tr>
+				<td><a href="#" onclick="(); return false;"  >あ</a></td>
+				<td><a href=""  >あ</a></td>
+				<td><a href=""  >あ</a></td>
+				<td><a href=""  >あ</a></td>
+				<td><a href=""  >あ</a></td>
+				<td><a href=""  >あ</a></td>
+				<td><a href=""  >リンク</a></td>
+			</tr>		
+		</table>
+	</form>
+	
+	<table class="button2">  
+		<tr>  
+			<td><a href="" >リンク</a></td>
+			<td><a href="" >リンク</a></td>  
+		</tr>
+		<tr> 
+			<td><a href="" >メール</a></td>
+			<td><a href="" >あ</a></td>  
+		</tr>
+		<tr>
+			<td><a href="http://b-cures.com/" >ホーム</a></td>
+			<td><a href="http://b-cures.com/" >ホーム</a></td>  
+		</tr>  
+	</table>
+	
+	</body>
+</html>
