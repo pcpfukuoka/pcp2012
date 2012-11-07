7f3b9969b67130bf2890ae2f3ebfba95877d8e05
shotaueki <o_hara1145081@mail.goo.ne.jp>
2012-11-07 15:48:37 +0900

    成績管理
    メイン画面更新

--- Results_management/res_main.php
+++ Results_management/res_main.php
@@ -1,1 +1,17 @@
-<?php
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" ></meta>
+		<title>成績管理メイン</title>
+	</head>
+	
+	<body>
+		<div align = "center">
+			<font size = "6">権限管理追加画面</font><hr><br><br><br>
+		</div>
+		
+		<input type = "button" onclick="jump('res_Att.php','right')" value = "欠席">
+		<input type = "button" onclick="jump('res_mini_test.php','right')" value = "ミニテスト">
+		<input type = "button" onclick="jump('res_test.php','right')" value = "定期テスト">
+		<input type = "button" onclick="jump('res_Att.php','right')" value = "成績">
+	</body>
+</html>
