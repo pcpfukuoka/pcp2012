e474c9df885c0e7d7963904c035cc67a84a81714
mizutachika <o_hara1145007@mail.goo.ne.jp>
2012-11-08 13:59:17 +0900

    中継画面２
    変更中

--- Print_delivery/pdf_relay.php
+++ Print_delivery/pdf_relay.php
@@ -2,4 +2,13 @@
 	session_start();
 	$user_seq = $_SESSION['login_info[user]'];
 	
+	//データベースの呼出
+	require_once("../lib/dbconect.php");
+	$dbcon = DbConnect();
+	
+	$id = $_GET['id'];
+	
+	//データベースを閉じる
+	Dbdissconnect($dbcon);
+	
 ?>
