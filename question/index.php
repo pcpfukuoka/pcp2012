d53302de6cd47917755eac31b4dbe7847cc0def7
shotaueki <o_hara1145081@mail.goo.ne.jp>
2012-11-12 00:08:50 +0900

    アンケート画面
    とりあえず使えないけど

---
+++ question/index.php
@@ -0,0 +1,15 @@
+<?php 
+	session_start();
+?>
+
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	</head>
+	<body>
+	<h1>アンケート管理</h1>
+	<li><a href = "list.php"><ul>アンケートリスト</ul></a></li>	
+	<li><a href = "regist_view.php"><ul>新規登録</ul></a></li>	
+	<li><a href = "answer_list"><ul>アンケート回答</ul></a></li>	
+	</body>
+</html>
