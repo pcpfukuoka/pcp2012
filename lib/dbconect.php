cf6b195f385d10b5ea6aa60a96fff3df735a3af6
shotaueki <o_hara1145081@mail.goo.ne.jp>
2012-11-16 11:15:15 +0900

    本番用
    コミット

--- lib/dbconect.php
+++ lib/dbconect.php
@@ -1,8 +1,8 @@
 <?php
     function DbConnect()
     {
-		$link = mysql_connect("10.50.202.105","root","");
-    	//$link = mysql_connect("49.212.201.99","pcp","pcp2012");
+		//$link = mysql_connect("10.50.202.105","root","");
+    	$link = mysql_connect("49.212.201.99","pcp","pcp2012");
 
 		mysql_select_db("pcp2012");
 
