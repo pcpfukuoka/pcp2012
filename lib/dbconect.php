5760c1d8e7d11b9567b1ed416259a21a5e2c33dc
shotaueki <o_hara1145081@mail.goo.ne.jp>
2012-11-25 15:25:28 +0900

    DB接続設定変更
    詳細はメールで

--- lib/dbconect.php
+++ lib/dbconect.php
@@ -1,8 +1,9 @@
 <?php
     function DbConnect()
     {
-		$link = mysql_connect("tamokuteki41","root","");
-    	//$link = mysql_connect("49.212.201.99","pcp","pcp2012");
+    	
+    	$dbconfig = parse_ini_file("config.ini");    	
+		$link = mysql_connect($dbconfig['address'],$dbconfig['user'],$dbconfig['pass']);
 
 		mysql_select_db("pcp2012");
 
