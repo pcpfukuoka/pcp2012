f035d670abddf81d6e389d5133a14146c176cf5f
mizutachika <o_hara1145007@mail.goo.ne.jp>
2012-11-30 15:39:33 +0900

    アンケートメイン画面
    権限管理の追加

--- question/index.php
+++ question/index.php
@@ -1,22 +1,46 @@
-<?php 
+<?php
 	session_start();
+
+	if(!isset($_SESSION["login_flg"]) || $_SESSION['login_flg'] == "false")
+	{
+		//header("Location:login/index.php");
+	}
+
+	//page_seq = 12(アンケート)
+	require_once("../lib/autho.php");
+	$page_fun = new autho_class();
+	$page_cla = $page_fun -> autho_Pre($_SESSION['login_info[autho]'], 12);
+
+	if($page_cla[0]['read_flg'] == 0)
+	{
+		header("Location:../top_left.php");
+	}
 ?>
 
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-				<link rel="stylesheet" type="text/css" href="../css/back_ground.css" />
+		<link rel="stylesheet" type="text/css" href="../css/back_ground.css" />
 		<link rel="stylesheet" type="text/css" href="../css/button.css" />
 		<link rel="stylesheet" type="text/css" href="../css/text_display.css" />
 		<script src="../javascript/frame_jump.js"></script>
-		
+
 	</head>
 	<body>
 	<h1>アンケート管理</h1>
-	
-	
-	<input class="button2" type="button" onclick="jump('list.php')" value="アンケートリスト">
-	<input class="button2" type="button" onclick="jump('regist_view.php')" value="新規登録">
-	<input class="button2" type="button" onclick="jump('answer_list.php')" value="アンケート回答">
+
+	<?php
+		if($page_cla['delivery_flg'] == 1)
+		{
+
+	?>
+			<input class="button2" type="button" onclick="jump('list.php')" value="アンケートリスト">
+			<input class="button2" type="button" onclick="jump('regist_view.php')" value="新規登録">
+
+	<?php
+		}
+	?>
+
+			<input class="button2" type="button" onclick="jump('answer_list.php')" value="アンケート回答">
 	</body>
 </html>
