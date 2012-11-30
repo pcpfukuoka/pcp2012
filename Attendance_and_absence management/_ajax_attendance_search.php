84848b047e760121721e132f21a780efb9fb3246
sakata <o_hara1045104@mail.goo.ne.jp>
2012-11-30 15:27:07 +0900

    座席管理
    編集

--- Attendance_and_absence management/_ajax_attendance_search.php
+++ Attendance_and_absence management/_ajax_attendance_search.php
@@ -1,7 +1,9 @@
 <?php
 
 session_start();
-$user_seq = $_POST['id'];
+$date = $_POST['date'];
+$group_seq = $_POST['group_seq'];
+
 require_once("../lib/dbconect.php");
 $link = DbConnect();
 $sql = "SELECT * FROM m_user WHERE delete_flg = 0";
@@ -9,12 +11,14 @@ $result = mysql_query($sql);
 $cnt = mysql_num_rows($result);
 
 
-$sql = "SELECT attendance.group_seq, attendance.user_seq, date, m_group.group_name AS group_name, m_user.user_name AS user_name, m_student.student_id AS student_id,
+$sql = "SELECT attendance.group_seq, attendance.user_seq, date,m_group.group_seq,m_group.group_name AS group_name, m_user.user_name AS user_name, m_student.student_id AS student_id,
 			   SUM(Attendance_flg) AS Attendance_flg, SUM(Absence_flg) AS Absence_flg, SUM(Leaving_early_flg) AS Leaving_early_flg, SUM(Lateness_flg) AS Lateness_flg, SUM(Absence_due_to_mourning_flg) AS Absence_due_to_mourning_flg
 		FROM attendance
 		LEFT JOIN m_group ON attendance.group_seq = m_group.group_seq
 		LEFT JOIN m_user ON attendance.user_seq = m_user.user_seq
 		LEFT JOIN m_student ON attendance.user_seq = m_student.user_seq
+		WHERE m_group.group_seq = '$group_seq'
+		AND DATE_FORMAT(attendance.date,'%Y-%m') = '$date'
 		GROUP BY attendance.user_seq;";
 $result = mysql_query($sql);
 $num = mysql_num_rows($result);
@@ -35,3 +39,5 @@ for ($i = 0; $i < $num; $i++)
 
 $test = json_encode($result_1);
 echo $test;
+
+
