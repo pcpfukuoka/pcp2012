ed16e3faba016243fb9c77fbe981308462f40e47
mizutachika <o_hara1145007@mail.goo.ne.jp>
2012-11-30 15:37:28 +0900

    プリント配信メイン画面
    権限管理の追加

--- Print_delivery/p_main.php
+++ Print_delivery/p_main.php
@@ -1,6 +1,23 @@
 <?php
 	session_start();
 	$user_seq = $_SESSION['login_info[user]'];
+
+	if(!isset($_SESSION["login_flg"]) || $_SESSION['login_flg'] == "false")
+	{
+		//header("Location:login/index.php");
+	}
+
+	//page_seq = 11(プリント配信)
+	require_once("../lib/autho.php");
+	$page_fun = new autho_class();
+	$page_cla = $page_fun -> autho_Pre($_SESSION['login_info[autho]'], 11);
+
+
+	if($page_cla[0]['read_flg'] == 0)
+	{
+		header("Location:../top_left.php");
+	}
+
 ?>
 
 <html>
