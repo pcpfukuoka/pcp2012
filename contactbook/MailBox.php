f9a898545a28b2b1ad76b57291230e65bc17fa1f
sakata <o_hara1045104@mail.goo.ne.jp>
2012-12-03 15:22:21 +0900

    連絡帳
    作成

--- contactbook/MailBox.php
+++ contactbook/MailBox.php
@@ -6,8 +6,8 @@
 	require_once("../lib/dbconect.php");
 	$dbcon = DbConnect();
 
-	//連らう帳のテータベースからデータの取り出し
-	$sql = "SELECT contact_book_seq, send_date, m_user.user_name AS send_user_name, title
+	//連絡帳のテータベースからデータの取り出し
+	$sql = "SELECT contact_book_seq, new_flg, send_date, m_user.user_name AS send_user_name, title
 			FROM contact_book
 			Left JOIN m_user ON contact_book.send_user_seq = m_user.user_seq
 			WHERE contact_book.reception_user_seq = $user_seq
@@ -47,6 +47,7 @@
 		<div align="center">
 			<table class="table_01">
 				<tr bgcolor="yellow">
+				<td align="center"width="30"></td>
 				<td align="center"width="200"><font size="5">日付</font></td>
 				<td align="center"width="150"><font size="5">FROM</font></td>
 				<td align="center"width="230"><font size="5">件名</font></td>
@@ -56,6 +57,20 @@
 					$row = mysql_fetch_array($result);
 				?>
 					<tr>
+
+					<?php
+
+						if ($row['new_flg'] == 1)
+						{
+					?>
+						<td><img src="../images/mail_icon.jpg"></td>
+					<?php
+						}
+						else
+						{
+							echo "<td></td>";
+						}
+					?>
 						<th><?= $row['send_date'] ?></th>
 						<th><?= $row['send_user_name'] ?></th>
 						<th>
@@ -78,12 +93,14 @@
 			$dbcon = DbConnect();
 
 			//プリント配信用のデータベースからデータの取り出し
-			$sql = "SELECT print_delivery_seq, target_group_seq, delivery_user_seq, delivery_date, printurl, title, m_user.user_name AS send_user_name
+			$sql = "SELECT print_delivery_seq, target_group_seq, print_flg, delivery_user_seq, delivery_date, printurl, title, m_user.user_name AS send_user_name
 					FROM print_delivery
 					LEFT JOIN m_user ON print_delivery.delivery_user_seq = m_user.user_seq
 					LEFT JOIN group_details ON print_delivery.target_group_seq = group_details.group_seq
 					WHERE group_details.user_seq = $user_seq
 					ORDER BY delivery_date DESC;";
+
+			echo $sql;
 			$result = mysql_query($sql);
 			$cnt = mysql_num_rows($result);
 
@@ -100,6 +117,7 @@
 		<div align="center">
 			<table class="table_01">
 				<tr bgcolor="yellow">
+					<td align="center"width="30"></td>
 					<td align="center"width="200"><font size="5">日付</font></td>
 					<td align="center"width="150"><font size="5">FROM</font></td>
 					<td align="center"width="230"><font size="5">件名</font></td>
@@ -110,6 +128,21 @@
 					$row = mysql_fetch_array($result);
 				?>
 					<tr>
+
+					<?php
+
+						if ($row['print_flg'] == 1)
+						{
+					?>
+						<td><img src="../images/mail_icon.jpg"></td>
+					<?php
+						}
+						else
+						{
+							echo "<td></td>";
+						}
+					?>
+
 						<th><?= $row['delivery_date'] ?></th>
 						<th><?= $row['send_user_name'] ?></th>
 						<th>
