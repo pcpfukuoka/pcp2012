578b1dc8cc68f6c5f8fd278d1e4bee158d976145
mizutachika <o_hara1145007@mail.goo.ne.jp>
2012-12-03 12:42:11 +0900

    返信
    編集

--- contactbook/ReplyBox.php
+++ contactbook/ReplyBox.php
@@ -10,23 +10,25 @@
 
 <html>
 	<head>
-	　　<title> 返信</title>
+	  <title>返信</title>
 	  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-		<meta http-equiv="Content-Style-Type" content="text/css">
-	    <link rel="stylesheet" type="text/css" href="../css/button.css" />
-	    <link rel="stylesheet" type="text/css" href="../css/back_ground.css" />
+	  <meta http-equiv="Content-Style-Type" content="text/css">
+	  <link rel="stylesheet" type="text/css" href="../css/button.css" />
+	  <link rel="stylesheet" type="text/css" href="../css/back_ground.css" />
+	  <link rel="stylesheet" type="text/css" href="../css/text_display.css" />
 	</head>
 
 	<body>
 		<img class="bg" src="../images/blue-big.jpg" alt="" />
 		<div id="container">
-		<form action="relay.php" method="POST" id="input">
-		  <div align="center">
-		    <font size = "7">返信</font><br>
-		  </div>
-		　　<hr color="blue">
-		  <br><br>
+		<div align="center">
+			<font class="Cubicfont">返信</font>
+		</div>
 
+		<hr color="blue">
+		<br><br><br>
+
+		<form action="relay.php" method="POST" id="input">
 		  <font size="3">宛先　： </font>
 		  <?= "$send_name"?><br>
 		  <font size="3">件名　： </font>
