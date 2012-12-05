50f6457e1d346b7f8c262757501067a6b72e86a9
AkiraKasahara <a_kasahara0821@yahoo.co.jp>
2012-12-05 18:05:58 +0900

    座席表
    所属するグループの表示に変更

---
+++ zaseki/seat_menu.php
@@ -0,0 +1,45 @@
+
+<?php
+	//session_start();
+
+	//$user = $_SESSION['login_info[user]'];
+	$user = 11;
+?>
+<html>
+	<head>
+		<title>seat_list</title>
+	</head>
+<?php
+	//データベースの呼出
+	require_once("../lib/dbconect.php");
+	$dbcon = DbConnect();
+	mysql_query("set names utf8");
+
+	//文字コード設定
+	mysql_query("SET NAMES UTF8");
+
+
+?>
+	<body>
+		<form action="seat_view2.php" method="POST">
+<?php
+		$sql = "select m_group.group_name,m_group.group_seq
+					from m_group,group_details
+						where group_details.user_seq = '$user'
+							and	 m_group.group_seq = group_details.group_seq";
+		$res = mysql_query($sql);
+?>
+			<select name="group" >
+<?php
+			while($gyo = mysql_fetch_array($res))
+			{
+?>
+				<option value=<?= $gyo['group_seq']?>> <?=  $gyo['group_name']?></option>
+<?php
+			}
+?>
+			</select>
+			<input type = "submit" value = "座席表">
+		</form>
+	</body>
+</html>
