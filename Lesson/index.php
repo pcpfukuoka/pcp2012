cb566ea4f853a4e0881a5806ff7aa82f2a84166a
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-12-05 12:24:54 +0900

    lesson
    一覧画面の作成

--- Lesson/index.php
+++ Lesson/index.php
@@ -1,44 +1,46 @@
 <?php
-session_start();
+	session_start();
+	require_once("../lib/autho.php");
+	$page_fun = new autho_class();
+	$page_cla = $page_fun -> autho_Pre($_SESSION['login_info[autho]'], 9);
 
-$user_seq = $_SESSION['login_info[user]'];
 
-require_once("../lib/dbconect.php");
-$dbcon = DbConnect();
-
-//自分のクラスの授業が今やっているか
-//時間割りができたら教科も検索条件に追加すること
-$sql = "SELECT class_seq,subject_seq 
-		FROM board
-		INNER JOIN group_details ON board.class_seq = group_details.group_seq
-		WHERE board.date = DATE_FORMAT(now(),'%Y-%m-%d')  AND group_details.user_seq = 1  ";
-
-$result = mysql_query($sql);
-$cnt = mysql_num_rows($result);
-
-if($cnt == 0)
-{
-	echo "a";
-	//別ベージに飛ばして授業が行われてないことを知らせる。
-}
-else
-{
-	$row = mysql_fetch_array($result);
-	$class_seq = $row['class_seq'];
-	//黒板ページに飛ばす
-	header("Location: 49.212.201.99:3000?id=$class_seq");
-
-	?>	
-<?php 
-
-}
+	if($page_cla[0]['read_flg'] == 0)
+	{
+		header("Location:../top_left.php");
+	}
 ?>
 
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+		<link rel="stylesheet" type="text/css" href="../css/back_ground.css" />
+		<link rel="stylesheet" type="text/css" href="../css/button.css" />
+		<link rel="stylesheet" type="text/css" href="../css/text_display.css" />
+		<script src="../javascript/frame_jump.js"></script>
+	</head>
+	<body>
+		<img class="bg" src="../images/blue-big.jpg" alt="" />
+		<div id="container">
+			<div align="center">
+				<font class="Cubicfont">授業（仮）</font>
+			</div>
+			<hr color="blue"></hr>
+			<br><br><br>
+			<p align="center">
+				<input type="button" onclick="jump('lesson/past_lesson.php','right')" value="過去の授業">
+				<br><br>
 
+				<!-- 生徒用の画面 -->
+				<input type="button" onclick="jump('lesson/now_lesson.php','right')" value="現在の授業">
+				<br><br>
 
+				<!-- 先生用の画面 -->
+				<input type="button"  value="授業開始">
+				<br><br>
+				<input type="button"  value="授業準備">
 
-
-
-
-
-
+			</p>
+		</div>
+	</body>
+</html>
