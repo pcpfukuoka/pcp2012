ea7b458ac1a09010d6e83728293567e81b2c41ae
mizutachika <o_hara1145007@mail.goo.ne.jp>
2012-12-05 15:55:25 +0900

    プリント配信
    データ混合の編集

--- Print_delivery/p_draft.php
+++ Print_delivery/p_draft.php
@@ -37,13 +37,13 @@
 		</div>
 
 		<hr color="blue">
-		<br><br><br>
+		<br><br>
 
 		<p align="center">
 			<div align="center">
 				<table class="table_01">
 					<tr bgcolor="yellow">
-					<td align="center"width="160"><font size="5">日付</font></td>
+					<td align="center"width="200"><font size="5">日付</font></td>
 					<td align="center"width="150"><font size="5">TO</font></td>
 					<td align="center"width="230"><font size="5">件名</font></td>
 
--- Print_delivery/p_relay.php
+++ Print_delivery/p_relay.php
@@ -7,32 +7,32 @@
 	$dbcon = DbConnect();
 
     //一時保存
-    if ( isset($_POST['Preservation']) )
+    if (isset($_POST['Preservation']))
     {
         //保存ボタンの時の処理
     	$data = $_FILES['pdf'];
     	$title = $_POST['title'];
     	$send_seq = $_POST['to'];
-    	
-    	$sql = "INSERT INTO Print_delivery (title, delivery_user_seq, target_group_seq, print_send_flg, print_flg, delivery_date)
+
+    	$sql = "INSERT INTO print_delivery (title, delivery_user_seq, target_group_seq, print_send_flg, print_flg, delivery_date)
     			VALUES ('$title', '$user_seq', '$send_seq', '1', '0', now())";
     	mysql_query($sql);
-    	
+
     	//INSERTしたSEQを取得
-    	$sql = "SELECT print_delivery_seq FROM Print_delivery ORDER BY print_delivery_seq DESC;";
+    	$sql = "SELECT print_delivery_seq FROM print_delivery ORDER BY print_delivery_seq DESC;";
     	$result = mysql_query($sql);
     	$row = mysql_fetch_array($result);
     	$pdseq = $row['print_delivery_seq'];
-    	
+
     	//UPDATEでリンクを保存
-    	$sql = "UPDATE Print_delivery SET printurl = 'print_delivery_seq_$pdseq.pdf'
+    	$sql = "UPDATE print_delivery SET printurl = 'print_delivery_seq_$pdseq.pdf'
     			WHERE print_delivery_seq = $pdseq;";
     	mysql_query($sql);
     	move_uploaded_file($data['tmp_name'], 'print_delivery_seq_$pdseq.pdf');
-    	 
+
     	//データベースを閉じる
     	Dbdissconnect($dbcon);
-    	
+
 		Header('Location: p_preservation.html');
         exit;
     }
@@ -45,30 +45,60 @@
     	$title = $_POST['title'];
     	$group_name = $_POST['group_name'];
     	$group_seq = $_POST['group_seq'];
-   		    	
-    	$sql = "UPDATE Print_delivery
+
+    	$sql = "UPDATE print_delivery
 				SET title = '$title', print_flg = 1, print_send_flg = 0, delivery_date = now()
 				WHERE Print_delivery_seq = '$print_delivery_seq'; ";
     	mysql_query($sql);
-    	    	
+
     	$sql = "SELECT * FROM group_details WHERE group_seq = '$group_seq'";
     	$group_result = mysql_query($sql);
     	$group_cnt = mysql_num_rows($group_result);
-    	
+
     	for ($i = 0; $i < $group_cnt; $i++)
     	{
     		$row = mysql_fetch_array($group_result);
-    		$group_user_seq = $row['user_seq']; 
-	    	//print_checkにデータをインサート
+    		$group_user_seq = $row['user_seq'];
+	    	//print_checkにデータをINSERT
 	    	$sql = "INSERT INTO print_check (print_delivery_seq, user_seq, print_check_flg)
 	    			VALUE ('$print_delivery_seq', '$group_user_seq', '1')";
 	    	mysql_query($sql);
     	}
-    	
+
     	//データベースを閉じる
     	Dbdissconnect($dbcon);
-    	 
+
     	Header('Location: p_com_disp.php');
+    	exit;
+    }
+    //保存からの保存
+    else if(isset($_POST['Re_preservation']))
+    {
+    	$title = $_POST['title'];
+    	$print_delivery_seq = $_POST['print_delivery_seq'];
+
+    	$sql = "UPDATE print_delivery
+    			SET title = '$title', delivery_date = now()
+    			WHERE print_delivery_seq = '$print_delivery_seq';";
+    	mysql_query($sql);
+
+    	//INSERTしたSQLを取得
+    	$sql = "SELECT print_delivery_seq FROM print_delivery ORDER BY print_delivery_seq DESC;";
+    	$result = mysql_query($sql);
+    	$row = mysql_fetch_array($result);
+    	$pdseq = $row['print_delivery_seq'];
+
+    	//UPDATEでリンクを保存
+    	$sql = "UPDATE print_delivery SET printurl = 'print_delivery_seq_$pdseq.pdf'
+    			WHERE print_delivery_seq = $pdseq;";
+    	mysql_query($sql);
+    	$file_name = "print_delivery_seq_" . $pdseq . ".pdf";
+    	move_uploaded_file($data['tmp_name'], $file_name);
+
+    	//データベースを閉じる
+    	Dbdissconnect($dbcon);
+
+    	Header('Location: p_preservation.html');
     	exit;
     }
 ?>
