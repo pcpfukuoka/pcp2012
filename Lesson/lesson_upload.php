1b5da1d36606b2aefa75b6ea5255f719d9203b00
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-12-06 16:32:22 +0900

    授業
    びちょうせ「　

--- Lesson/lesson_upload.php
+++ Lesson/lesson_upload.php
@@ -20,7 +20,7 @@ session_start();
 	echo $date;
 
 
-	$sql = "INSERT INTO board VALUE(0,".$date .", '15','15',".$page_num .",'0','0',". $img_name . ");";
+	$sql = "INSERT INTO board VALUE(0,'".$date ."', '15','15','".$page_num ."','0','0','". $img_name . "');";
 	echo $sql;
 	$result = mysql_query($sql);
 	$file_name = 'files/ '.$img_name;
