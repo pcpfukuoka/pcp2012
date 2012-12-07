7b65474eba9e033c7ccffc040564b922c0c4f3d2
kobayashiaoi <kobayashiaoi9@yahoo.co.jp>
2012-12-07 12:42:24 +0900

    座席表
    登録機能修正

--- zaseki/seat_register_step2.php
+++ zaseki/seat_register_step2.php
@@ -50,14 +50,24 @@
 
     $(function() {
 
+    	//入れ替えボタンを押せなくする
+		$('#change').attr("disabled", "disabled");
+
 		$('#add').click(function(){
+
+			//追加ボタンを押せなくする
+			$('#add').attr("disabled", "disabled");
+
+			//入れ替えボタンを押せるようにする
+			$('#change').removeAttr("disabled");
 
 			//セルの背景色をすべて戻す
 			$('.seat').attr({"bgcolor": "white"});
 
 			//一番最初の空白の背景色を変える
-			for(var seat_id = 1; seat_id <= 100; seat_id++)
+			for(var i = 1; i <= 100; i++)
 			{
+				seat_id = i;
 				if($('#'+seat_id).children('p').text() == "")
 				{
 					$('#'+seat_id).attr({"bgcolor": "red"});
@@ -70,6 +80,12 @@
 	    });
 
 		$('#change').click(function(){
+
+			//追加ボタンを押せるようにする
+			$('#add').removeAttr("disabled");
+
+			//入れ替えボタンを押せなくする
+			$('#change').attr("disabled", "disabled");
 
 			//セルの背景色をすべて戻す
 			$('.seat').attr({"bgcolor": "white"});
@@ -119,40 +135,49 @@
 
 		$('.seat').click(function(){
 
+			//追加ボタンが押されている時
 			if(mode == "add")
 			{
 				if($(this).children("p").text() != "")
 				{
+					//セルをクリアしてリストの元の位置に戻す
 					list_id = $(this).children('input:eq(1)').val();
 					$('#'+list_id).children('p').text($(this).children("p").text());
 					$(this).children('p').text("");
 				}
 				else
 				{
+					//クリックしたセルを選択状態にする
 					$('.seat').attr({"bgcolor": "white"});
 					seat_id = $(this).attr("id");
 					$(this).attr({"bgcolor": "red"});
 				}
 
 			}
+			//入れ替えボタンが押されている時
 			else if(mode == "change")
 			{
-				if(id_evc == 0)
+				if(evc_flg == 0)
 				{
-						//一回目にクリックしたセルのデータを保存
+						//一回目にクリックしたセルのデータを退避させる
 						id_evc = $(this).attr("id");
 						name_evc = $(this).children("p").text();
 						user_seq_evc = $(this).children().val();
 
 						//一回目にクリックしたセルの色を変える
 						$(this).attr({"bgcolor": "yellow"});
+
+						//退避フラグをONにする
+						evc_flg = 1;
 			    }
 				else
 				{
 
+						//一回目にクリックしたセルに二回目にクリックしたデータを移す
 						$('#' + id_evc).children("p").text($(this).children("p").text());
 						$('#' + id_evc).children().attr({"value": $(this).children().val()});
 
+						//二回目にクリックしたセルに一回目に退避したデータを移す
 						$(this).children("p").text(name_evc);
 						$(this).children().attr({"value": user_seq_evc});
 
@@ -160,11 +185,10 @@
 						//セルの色を戻す
 						$('#' + id_evc).attr({"bgcolor": ""});
 
-						seat_id = 0;
 						id_evc = 0;
 						name_evc = "";
 						user_seq_evc = "";
-						attendance_no2 = ""
+						evc_flg = 0;
 				}
 			}
 	    });
