97d0162b5df6becb7fcf7f6ecc10ddbf0980c0a7
taniguchiyukitada <taniguchi106091@gmail.com>
2012-12-07 10:21:00 +0900

    カレンダー
    カレンダー

---
+++ dl_calendar/calendar_main.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+		<script src="../javascript/frame_jump.js"></script>
+		<meta http-equiv="Content-Style-Type" content="text/css">
+	    <link rel="stylesheet" type="text/css" href="../css/button.css" />
+		<link rel="stylesheet" type="text/css" href="../css/text_display.css" />
+		<link rel="stylesheet" type="text/css" href="../css/back_ground.css" />
+		<script src="../javascript/frame_jump.js"></script>
+		<title>カレンダーメイン</title>
+	</head>
+	<body>
+		<img class="bg" src="../images/blue-big.jpg" alt="" />
+		<div id="container">
+		<div align="center">
+
+		<br>
+			<font class="Cubicfont1">メイン画面</font>
+		</div>
+		<hr color="blue">
+
+		<br><br><br>
+		<p  align="center">
+			<input class="button2" type="button" onclick="jump('calendar.php')" value="カレンダー確認">
+			<br><br>
+			<input class="button2" type="button" onclick="jump('menu.php')" value="献立表確認">
+		</p>
+
+
+		</div>
+	</body>
+</html>
