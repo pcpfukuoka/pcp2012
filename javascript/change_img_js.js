ef09e9c33132831abd6a5916fe0cf12212e433d2
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-12-13 10:50:22 +0900

    授業関連
    ミス調整

--- javascript/change_img_js.js
+++ javascript/change_img_js.js
@@ -1,6 +1,6 @@
 function change_img(img_url,img_page_num){
 	var image_tag = img_page_num + "_image";
 	var im = parent.document.getElementById(image_tag);
-	im.src = img_tag_name;
+	im.src = img_url;
 
 }
