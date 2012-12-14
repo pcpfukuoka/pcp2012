4aa724e44263828ed0c45f291e8752aaebd09789
kobayashiaoi <kobayashiaoi9@yahoo.co.jp>
2012-12-14 14:57:42 +0900

    献立表
    変更、修正

--- dl_calendar/menu.php
+++ dl_calendar/menu.php
@@ -3,6 +3,7 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="../css/back_ground.css" />
+	<link rel="stylesheet" type="text/css" href="../css/button.css" />
 	<link rel="stylesheet" type="text/css" href="../css/text_display.css" />
 	<script type="text/javascript" src="../javascript/winOpen.js"></script>
 	<title>こんだて</title>
@@ -15,14 +16,15 @@
 	<Div Align="center">
 
 		<DIV style="top : 15px;left : 20px; position : absolute;">
-			<input type="button"  value="献立表変更" onClick="upload();">
+			<input type="button"  value="献立表変更" onClick="upload();" class = "button3";>
 		</DIV>
 
-		<DIV style="top : 15px; left : 600px; position : absolute;">
-			<a href="menu.php"><input type="button" value="画面更新"></a>
+		<DIV style="top : 15px;left : 580px; position : absolute;">
+			<a href="menu.php"><input type="button" value="画面更新" class = "button3"></a>
 		</DIV>
 
-		<font class = "Cubicfont">今月のこんだて</font><br><br><br>
+		<font class = "Cubicfont">今月のこんだて</font><hr color="blue">
+		<br><br><br>
 
 			<img src="menu.jpg" alt="menu" width=650>
 			<br><br>
