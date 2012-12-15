727f616e260a8eff0e5b8adfcd0fafff2bd03fbf
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-12-15 16:50:28 +0900

    過去授業
    テスト

--- Lesson/old_lesson_view.php
+++ Lesson/old_lesson_view.php
@@ -1,20 +1,26 @@
 
 <?php
 
-$date = $_POST['date'];
+	$date = $_POST['date'];
 
-$subject_seq = $_POST['subject_seq'];
+	$subject_seq = $_POST['subject_seq'];
 
-//データベースの呼出
-require_once("../lib/dbconect.php");
-$dbcon = DbConnect();
+	//データベースの呼出
+	require_once("../lib/dbconect.php");
+	$dbcon = DbConnect();
 
-//$sql = "SELECT div_url, canvas_url FROM board WHERE date=". $date ."subject_seq=".$subject_seq .";";
-$sql = "SELECT div_url FROM board WHERE date='". $date ."' AND subject_seq=15;";
-$result = mysql_query($sql);
+	//$sql = "SELECT div_url, canvas_url FROM board WHERE date=". $date ."subject_seq=".$subject_seq .";";
+	$sql = "SELECT div_url FROM board WHERE date='". $date ."' AND subject_seq=15;";
+	$result = mysql_query($sql);
 
-$count = mysql_num_rows($result);
+	$count = mysql_num_rows($result);
 
+	for($i = 0;i<$count;$i++){
+
+		$row = mysql_fetch_array($result);
+		$div_arrangement[$i]= $row['div_url'];
+		$canvas_arrangement[$i]= $row['canvas_url'];
+	}
 ?>
 <html>
 <head>
@@ -22,7 +28,21 @@ $count = mysql_num_rows($result);
 
 <body>old_lesson_view,php
 <?php
-	?>
+	for($i = 0;i<$count;$i++){
+
+		$row = mysql_fetch_array($result);
+		$div_arrangement[$i]= $row['div_url'];
+		$canvas_arrangement[$i]= $row['canvas_url'];
+		$aaa = substr($row['div_url'],30);
+		$bbb = "../../balckboard/public".$aaa;
+		echo $bbb;
+?>
+	<div id="div_background" style="background: <?= $bbb ?>">
+
+	</div>
+<?php
+	}
+?>
 
 </body>
 </html>
