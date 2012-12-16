2e5b3e76d410f680a7036e47ca20d787d441eaee
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-12-16 13:50:20 +0900

    過去授業
    テスト

--- Lesson/old_lesson_view.php
+++ Lesson/old_lesson_view.php
@@ -9,8 +9,8 @@
 	require_once("../lib/dbconect.php");
 	$dbcon = DbConnect();
 
-	//$sql = "SELECT div_url, canvas_url FROM board WHERE date=". $date ."subject_seq=".$subject_seq .";";
-	$sql = "SELECT div_url FROM board WHERE date='". $date ."' AND subject_seq=15;";
+	$sql = "SELECT div_url, canvas_url FROM board WHERE date=". $date ."subject_seq=".$subject_seq .";";
+	//$sql = "SELECT div_url FROM board WHERE date='". $date ."' AND subject_seq=15;";
 	$result = mysql_query($sql);
 
 	$count = mysql_num_rows($result);
