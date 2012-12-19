542709737b3ba41e9aa51bafc62364d0fd0d8e25
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-12-19 18:21:36 +0900

    過去授業ｊｓ
    新規作成

---
+++ javascript/old_lesson_canvas_js.js
@@ -0,0 +1,47 @@
+function turn(div,canvas){
+	var page=getElementById('page_num');
+	var div=getElementById('chalkboard');
+	var canvas=getElementById('canvas');
+
+
+	var div_list=div.split(",");
+	var canvas_list=canvas.split(",");
+
+	//戻ったためひとつ前にカーソルをそろえる
+	page=page-1;
+
+	if(page==0){
+		page=0;
+	}
+
+	div.style.background=div_list[page];
+
+
+	canvas.src=canvas_list[page];
+
+	page.value=page;
+}
+
+function next(div,canvas){
+
+	var page=getElementById('page_num');
+	var div=getElementById('chalkboard');
+	var canvas=getElementById('canvas');
+
+
+	var div_list=div.split(",");
+	var canvas_list=canvas.split(",");
+	var list_num=div_list.length;
+
+	//戻ったためひとつ前にカーソルをそろえる
+	page=page+1;
+
+	if(page > list_num-1){
+		page=list_num-1;
+	}
+
+	div.style.background=div_list[page];
+	canvas.src=canvas_list[page];
+
+	page.value=page;
+}
