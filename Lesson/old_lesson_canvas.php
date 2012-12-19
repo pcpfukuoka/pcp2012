a146aaf5232bfd38efaf6b341ab48640e4fd08d2
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-12-19 12:20:27 +0900

    過去授業
    テスト

--- Lesson/old_lesson_canvas.php
+++ Lesson/old_lesson_canvas.php
@@ -13,64 +13,53 @@
 
 	$count = mysql_num_rows($result);
 
-	for($i = 1;$i <= $count;$i++){
+	$div_str = "";
+	$canvas_str = "";
 
+	for($i = 0;$i < $count;$i++){
 		$row = mysql_fetch_array($result);
 
 		$aaa = substr($row['div_url'],30);
-		$div[$i-1] ="url(../../balckboard/public/".$aaa;
-		$canvas[$i-1]=$row['canvas_url'];
+		$div[$i] ="url(../../balckboard/public/".$aaa;
+		$canvas[$i]=$row['canvas_url'];
 
 
-
+		if($i < $count-1){
+			$div_str = $div_str.$div[$i].",";
+			$canvas_str=$canvas_str.$canvas[$i].",";
+		}
+		else{
+			$div_str = $div_str.$div[$i];
+			$canvas_str=$canvas_str.$canvas[$i];
+		}
 	}
 
+	Dbdissconnect($dbcon);
 
 ?>
 
 	<!DOCTYPE html>
-	<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
-	<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
-	<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
-	<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
 		<meta name="viewport" content="width=1024, initial-scale=1, maximum-scale=1, user-scalable=no" />
-		<meta name="msapplication-task" content="Japanese Chalkboard">
-		<meta property="og:image" content="images/screenshot_small.png"/>
-		<meta property="og:title" content="Japanese Chalkboard (for IE10 Test Drive)"/>
 		<meta name="description" content="This is an example using HTML5 WebSocket. Multiple users can write this chalkboard at the same time.">
 
 		<link rel="stylesheet" href="../css/old_lesson_style.css">
 		<link rel="stylesheet" href="../css/old_lesson_index.css">
 		<script src="../js/modernizr-2.0.6.min.js"></script>
-			 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-			 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
-
-
 
 	</head>
 	<body>
-
-
-
 			<div id="chalkboard" style="background:<?= $div[0] ?>;background-repeat:no-repeat">
-				<img src="<?=$canvas[0] ?>">
+				<img src="<?=$canvas[0] ?>"id="canvas">
 			</div>
-			<input id="turn" value="戻る" type="button">
-	 		<input id="next" value="次へ"type="button">
+				<input id="turn" value="戻る" type="button" onclick="turn(<?=$div_str ?>,<?=$canvas_str ?>)">
+	 			<input id="next" value="次へ"type="button"onclick="next(<?=$div_str ?>,<?=$canvas_str ?>)">
+	 			<input type="hidden" value="0" id="page_num">
+
 	 		<br>
-
-		</div>
-	</div> <!--! end of #container -->
-
-	<script>window.jQuery || document.write('<script src="../js/jquery-1.7.1.min.js"><\/script>')</script>
-	<script src="../js/jquery.i18n.properties-min-1.0.9.js"></script>
-
-
-	<script src="../javascript/common.js"></script>
-
 	</body>
 </html>
