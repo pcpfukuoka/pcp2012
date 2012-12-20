2303398ce43f4edd11f75639c7e4462e46d92169
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-12-21 01:06:03 +0900

    過去授業
    新規作成

---
+++ javascript/old_lesson_js.js
@@ -0,0 +1,11 @@
+
+
+function decision_click(){
+	var date_ele=document.getElementById('date');
+	var subject_ele=document.getElementById('subject_seq');
+
+	var date=date_ele.value;
+	console.log(date);
+	var subject_seq=subject_ele.value;
+	console.log(subject_seq);
+}
