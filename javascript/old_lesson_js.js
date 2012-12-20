0e07e415785eca2f1d977da27d44468eb9be8314
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-12-21 01:11:38 +0900

    過去授業
    テスト

--- javascript/old_lesson_js.js
+++ javascript/old_lesson_js.js
@@ -8,4 +8,6 @@ function decision_click(){
 	console.log(date);
 	var subject_seq=subject_ele.value;
 	console.log(subject_seq);
+
+	window.open('old_lesson_canvas.php?date='.date'&id='.subject_seq, "old_lesson", "width=1000,height=1000");
 }
