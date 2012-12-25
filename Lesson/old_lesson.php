cb79769dfb0d4ae9898d093b38c5aaa3aa0827a0
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-12-26 01:16:13 +0900

    過去授業
    エラー訂正

--- Lesson/old_lesson.php
+++ Lesson/old_lesson.php
@@ -48,15 +48,14 @@
 			var subject_seq=subject_ele.value;
 
 			$('#frame').empty();
-			$.post('ajax_canvas_select.php', {
+			$.post('ajax_canvas_select.php',{
 		        date: date,
 		        id : subject_seq
 		    },
-		    //戻り値として、user_seq受け取る
 		    function(rs) {
 		    	var parsers = JSON.parse(rs);
-		    	var e='<div id="chalkboard" style="background:'+parsers[0][div]+';background-repeat:no-repeat">'
-		    	+'<img src="'+parsers[0][div]+'"id="canvas" />'
+		    	var e='<div id="chalkboard" style="background:'+parsers[0]['div']+';background-repeat:no-repeat">'
+		    	+'<img src="'+parsers[0]['canvas']+'"id="canvas" />'
 		    	+'</div>'
 		    	+'<input id="turn" value="戻る" type="button">'
 		    	+'<input id="next" value="次へ"type="button">'
