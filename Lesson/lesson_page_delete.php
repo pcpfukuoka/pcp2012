8170c31b7d39db6e596466ae714a5b427eb7c0ff
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-12-31 03:20:14 +0900

    授業準備
    テスト

--- Lesson/lesson_page_delete.php
+++ Lesson/lesson_page_delete.php
@@ -14,6 +14,7 @@
 	$delete_sql = "DELETE FROM board WHERE date='".$date."' AND subject_seq='".$subject_seq."'AND page_num >=".$page_num.";";
 
 	$delete_ = mysql_query($delete_sql);
+	$result_1 = array();
 
 	for($i= 0;$i<$count;$i++){
 		$row = mysql_fetch_array($result);
@@ -22,10 +23,11 @@
 		$max_page= $new_page_num;
 		$sql = "INSERT INTO board VALUE(0,'".$date ."', '15','".$subject_seq ."','".$new_page_num ."','".$row['div_url']."','0','0');";
 		$result = mysql_query($sql);
+		$result_1[] = array('div'=>$row['div_url'],'page_num'=>$row['page_num']);
 	}
 
 	Dbdissconnect($dbcon);
-	$test = json_encode($row);
+	$test = json_encode($relult_1);
 	echo $test;
 
 
