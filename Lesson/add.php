26b84126ddec55e62e5bc8e8498c5e78232da7fa
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-12-30 19:30:29 +0900

    授業準備
    テスト

--- Lesson/add.php
+++ Lesson/add.php
@@ -18,8 +18,31 @@
 <html>
 	<head>
 			<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-			<script src="../javascript/lesson_add_javascript.js"></script>
 	</head>
+	<script>
+	function delete_img(page_num){
+
+		var date_ele=document.getElementById('date');
+		var subject_ele=document.getElementById('subject_seq');
+		var page = page_num;
+		//日付と科目を変数に格納
+		var date=date_ele.value;
+		var subject_seq=subject_ele.value;
+		$.post('lesson_page_delete.php',{
+	        date: date,
+	        id : subject_seq,
+	        num : page
+	    },
+	    function(rs) {
+		    //ＦＯＲＭを削除
+	    	var delete_page = page_num+"_form";
+	    	delete_page.remove();
+	    	page_num++;
+	    });
+	}
+
+
+	</script>
 
 	<body>
 
