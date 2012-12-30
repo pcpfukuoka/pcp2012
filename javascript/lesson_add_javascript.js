f2de44051769943a5737b8a3358f7d9a822023df
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-12-30 19:20:11 +0900

    授業準備
    テスト

--- javascript/lesson_add_javascript.js
+++ javascript/lesson_add_javascript.js
@@ -1,20 +1,23 @@
-function delete_img(page_num){
 
-	var date_ele=document.getElementById('date');
-	var subject_ele=document.getElementById('subject_seq');
 
-	//日付と科目を変数に格納
-	var date=date_ele.value;
-	var subject_seq=subject_ele.value;
-	var id_name=page_num+"_delete";
-	$(id_name).remove();
-	$.post('ajax_canvas_select.php',{
-        date: date,
-        id : subject_seq
-        num : page_num
-    },
-    function(rs) {
-    	var parsers = JSON.parse(rs);
-    	$('#frame').append(e);
-    });
-}
+		function delete_img(page_num){
+
+			var date_ele=document.getElementById('date');
+			var subject_ele=document.getElementById('subject_seq');
+			var page = page_num;
+			//日付と科目を変数に格納
+			var date=date_ele.value;
+			var subject_seq=subject_ele.value;
+			$.post('lesson_page_delete.php',{
+		        date: date,
+		        id : subject_seq
+		        num : page
+		    },
+		    function(rs) {
+			    //ＦＯＲＭを削除
+		    	var delete_page = page_num+"_form";
+		    	delete_page.remove();
+		    	page_num++;
+		    });
+		}
+
