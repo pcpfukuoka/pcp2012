f18637114b1b9be2fa9f851ddb7fdcdf78848955
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-01-10 22:35:53 +0900

    授業準備
    テスト

--- Lesson/chatroom.php
+++ Lesson/chatroom.php
@@ -5,7 +5,10 @@ $time = time() + 60 * 60*24;
 
 setcookie("user_seq","12",$time,"/");
 setcookie("subject_seq","15",$time,"/");
-setcookie("flg","false",$time,"/");
+
+
+
+setcookie("flg","true",$time,"/");
 
 header("Location:http://49.212.201.99:3000");
 
