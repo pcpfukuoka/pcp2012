b6f106c0a5f00bfa8de087e88b8362f6e7a02bed
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-01-10 11:37:29 +0900

    チャットルーム
    テスト

--- Lesson/chatroom.php
+++ Lesson/chatroom.php
@@ -4,8 +4,8 @@ $time = time() + 60 * 60*24;
 
 
 setcookie("user_seq","12",$time,"/");
-setcookie("subject_seq","15","12",$time,"/");
-setcookie("flg","false","12",$time,"/");
+setcookie("subject_seq","15",$time,"/");
+setcookie("flg","false",$time,"/");
 
 header("Location:http://49.212.201.99:3000");
 
