05407d80b777a01bd09ebd6e6ca41221dfee1a6d
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-01-15 22:09:09 +0900

    授業準備
    エラー訂正

--- javascript/test_lesson_upload_js.js
+++ javascript/test_lesson_upload_js.js
@@ -30,10 +30,9 @@ function form_create(date,page_num,subject_seq,img_tag_name){
 	var page_ele=parent.document.getElementById(page_name);
 
 	//selectboxにページ数の追加
+	var change_sel=parent.document.getElementById("page_num_change");
 	var option='<option value="'+pa+'" id="'+pa+'_op">'+pa+'</option>'
-	$("#page_num_change").append(option);
-	$("#page_num_del").append(option);
-
+	change_sel.append(e);
 	pa++;
 	page_ele.value=pa;
 	page_ele.id=pa+"_page";
