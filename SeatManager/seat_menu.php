7d6b28425461d2206ea6cdfbf9477c37fd8534c8
kobayashiaoi <kobayashiaoi9@yahoo.co.jp>
2013-01-18 12:24:28 +0900

    座席表
    データベース接続の修正

--- SeatManager/seat_menu.php
+++ SeatManager/seat_menu.php
@@ -62,50 +62,9 @@
 			<input type = "submit" value = "席替え">
 		</form>
 
-
-		<!-- テスト用 -->
-
-				<form action="seat_view.php" method="POST">
-<?php
-		$sql = "select m_group.group_name,m_group.group_seq
-					from m_group";
-		$res = mysql_query($sql);
-?>
-			<select name="group" >
-<?php
-			while($gyo = mysql_fetch_array($res))
-			{
-?>
-				<option value=<?= $gyo['group_seq']?>> <?=  $gyo['group_name']?></option>
-<?php
-			}
-?>
-			</select>
-			<input type = "submit" value = "テスト座席表">
-		</form>
-		<form action="seat_change.php" method="POST">
-<?php
-		$sql = "select m_group.group_name,m_group.group_seq
-					from m_group";
-		$res = mysql_query($sql);
-?>
-			<select name="group" >
-<?php
-			while($gyo = mysql_fetch_array($res))
-			{
-?>
-				<option value=<?= $gyo['group_seq']?>> <?=  $gyo['group_name']?></option>
-<?php
-			}
-?>
-			</select>
-			<input type = "submit" value = "テスト席替え">
-		</form>
-
 		<form action="seat_register_step1.php">
 			<input type="submit"value="座席登録">
 		</form>
 
-		<!-- テスト用 -->
 	</body>
 </html>
