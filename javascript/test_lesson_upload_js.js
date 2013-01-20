7074a61f99d373d121cdcaca073c44efbd83e292
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-01-20 22:39:44 +0900

    授業
    エラー

--- javascript/test_lesson_upload_js.js
+++ javascript/test_lesson_upload_js.js
@@ -33,7 +33,7 @@ function form_create(date,page_num,subject_seq,img_tag_name){
 		tr_val=tr_val+1+"_tr";
 		var tr_=document.createElement("tr");
 		tr_.setAttribute("id",tr_val);
-		tbody.append(tr_);
+		table_ele.appendChild(tr_);
 	}
 
 	var page_name=pa+"_page";
