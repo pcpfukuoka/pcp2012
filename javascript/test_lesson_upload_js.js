396850f061f9c7456cf730cabaf87920fe65df8d
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-01-20 22:35:31 +0900

    授業
    エラー

--- javascript/test_lesson_upload_js.js
+++ javascript/test_lesson_upload_js.js
@@ -9,6 +9,9 @@ function form_create(date,page_num,subject_seq,img_tag_name){
 
 	//imageを追加するtrタグを取得
 	var insert_tr=Math.floor(pa/5);
+	if(insert_tr==0){
+		insert_tr++;
+	}
 	var tr_name=insert_tr+"_tr";
 	var tr_ele=parent.document.getElementById(tr_name);
 
