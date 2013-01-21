4b3a5ee8680d094866e6a0c8df4646b5ac70df8e
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-01-21 19:40:50 +0900

    授業
    エラー

--- javascript/test_lesson_upload_js.js
+++ javascript/test_lesson_upload_js.js
@@ -42,7 +42,7 @@ function form_create(date,page_num,subject_seq,img_tag_name){
 
 		var next_th=page_num+1;
 		var e="<th width='100'><font size='3'>"+next_th+"</font></th>";
-		var next_th++;
+		next_th++;
 		for(i=1;i<5;i++){
 			e=e+"<th width='100'><font size='3'>"+next_th+"</font></th>";
 			next_th++;
