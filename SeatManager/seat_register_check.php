0b841ba514ccf5a269e4bf3493d5ce15e42f6ff7
AkiraKasahara <a_kasahara0821@yahoo.co.jp>
2013-01-23 06:12:14 +0900

    座席表
    登録機能・新規

---
+++ SeatManager/seat_register_check.php
@@ -0,0 +1,71 @@
+<?php
+	$row_max = $_POST['row_max'];
+	$col_max = $_POST['col_max'];
+	$group = $_POST['group'];
+
+	//データベースの呼出
+	require_once("../lib/dbconect.php");
+	$dbcon = DbConnect();
+
+	//文字コード設定
+	mysql_query("SET NAMES UTF8");
+?>
+
+<html>
+	<head>
+		<title>user_select</title>
+		<script src="../sp/js/jquery-1.8.2.min.js"></script>
+	</head>
+	<body>
+<?php
+
+	$user_cnt = 0;
+	while($_POST['user'][$user_cnt])
+	{
+		$user_seq[$user_cnt] = $_POST['user'][$user_cnt];
+		$user_cnt++;
+	}
+?>
+
+	<form action="seat_register_add.php" method="POST">
+	<table border="1">
+<?php
+	for($row = 1; $row <= $row_max; $row++)
+	{
+?>
+		<tr>
+<?php
+		$seat_id = $row - 1;
+		for($col = 1; $col <= $col_max; $col++)
+		{
+			$user_name ="";
+
+			if($user_seq[$seat_id] != "")
+			{
+				$sql = "select user_name from m_user where user_seq = '$user_seq[$seat_id]'";
+				$res = mysql_query($sql);
+				$gyo = mysql_fetch_array($res);
+				$user_name = $gyo['user_name'];
+			}
+?>
+			<td id="<?=$seat_id?>" class='seat'>
+			<p><?= $user_name ?></p>
+			<input name = user_seq<?= $row?>[<?= $col?>] type="hidden" value = "<?= $user_seq[$seat_id] ?>">
+			<input type="hidden" value="">
+			</td>
+<?php
+			$seat_id = $seat_id + $row_max;
+		}
+?>
+		</tr>
+<?php
+	}
+?>
+	</table>
+
+	<input name="row_max" type="hidden" value= "<?= $row_max ?>">
+	<input name="col_max" type="hidden" value= "<?= $col_max ?>">
+	<input name= "group" type="hidden" value= "<?= $group ?>">
+	<input type="submit" value="登録">
+	</body>
+</html>
---
+++ SeatManager/seat_register_user_select.php
@@ -0,0 +1,212 @@
+<?php
+	//データベースの呼出
+	require_once("../lib/dbconect.php");
+	$dbcon = DbConnect();
+
+	//文字コード設定
+	mysql_query("SET NAMES UTF8");
+
+	$group = $_POST['group'];
+
+
+?>
+
+<html>
+	<head>
+		<title>user_select</title>
+		<script src="../sp/js/jquery-1.8.2.min.js"></script>
+
+		<style>
+
+			.left_box{
+				float:left;
+			}
+			.right_box{
+				float:left;
+			}
+			.user_list {
+				width:300px;
+				height:450px;
+				overflow:scroll;
+			}
+	</style>
+	</head>
+	<body>
+
+	<script>
+	 	 $(function() {
+
+			var $user_cnt = $("input[type=checkbox]:checked").length;
+			$('#user_cnt').text($user_cnt + "人の生徒が選択されています");
+
+			var $row_max = $('#row_max').val();
+			var $col_max = $('#col_max').val();
+			var $seat_cnt = $row_max * $col_max;
+			$('#seat_cnt').text($seat_cnt + "人分の座席があります");
+
+			var $balance = $seat_cnt - $user_cnt;
+			if($balance < 0)
+			{
+				$balance = $balance * -1;
+				$('#balance').text($balance + "人分の座席が不足しています");
+				$('#next').attr("disabled", "disabled");
+			}
+			else
+			{
+				$('#balance').text($balance + "人分の座席が余っています");
+			}
+
+
+
+
+
+			$("input[type=checkbox]").click(function(){
+				$user_cnt = $("input[type=checkbox]:checked").length;
+				$('#user_cnt').text($user_cnt + "人の生徒が選択されています");
+
+				$balance = $seat_cnt - $user_cnt;
+
+				if($balance < 0)
+				{
+					$balance = $balance * -1;
+					$('#balance').text($balance + "人分の座席が不足しています");
+					$('#next').attr("disabled", "disabled");
+				}
+				else
+				{
+					$('#balance').text($balance + "人分の座席が余っています");
+					$('#next').removeAttr("disabled");
+				}
+
+			});
+
+			$("#row_max").click(function(){
+				$row_max = $('#row_max').val();
+				$seat_cnt = $row_max * $col_max;
+				$('#seat_cnt').text($seat_cnt + "名分の座席があります");
+
+				$balance = $seat_cnt - $user_cnt;
+				if($balance < 0)
+				{
+					$balance = $balance * -1;
+					$('#balance').text($balance + "人分の座席が不足しています");
+					$('#next').attr("disabled", "disabled");
+				}
+				else
+				{
+					$('#balance').text($balance + "人分の座席が余っています");
+					$('#next').removeAttr("disabled");
+				}
+
+			});
+
+			$("#col_max").click(function(){
+				$col_max = $('#col_max').val();
+				$seat_cnt = $row_max * $col_max;
+				$('#seat_cnt').text($seat_cnt + "名分の座席があります");
+
+				$balance = $seat_cnt - $user_cnt;
+				if($balance < 0)
+				{
+					$balance = $balance * -1;
+					$('#balance').text($balance + "人分の座席が不足しています");
+					$('#next').attr("disabled", "disabled");
+				}
+				else
+				{
+					$('#balance').text($balance + "人分の座席が余っています");
+					$('#next').removeAttr("disabled");
+				}
+
+			});
+
+    	});
+    </script>
+
+<div class="left_box">
+	<form action="seat_register_check.php" method="POST">
+	<FIELDSET style="width:300px">
+	    <LEGEND>
+	    <b>メッセージ</b>
+	    </LEGEND>
+			<p id ="user_cnt">100<p>
+			<p id ="seat_cnt">100</p>
+			<p id = "balance"></p>
+	</FIELDSET>
+
+	<FIELDSET style="width:300px">
+	    <LEGEND>
+	    <b>座席数選択</b>
+	    </LEGEND>
+	    	<table>
+	    		<tr>
+	    			<td><p>横列：</p></td>
+	    			<td>
+		    			<SELECT id="row_max" name="row_max">
+							<OPTION value="1">１</OPTION>
+							<OPTION value="2">２</OPTION>
+							<OPTION value="3">３</OPTION>
+							<OPTION value="4">４</OPTION>
+							<OPTION value="5">５</OPTION>
+							<OPTION value="6" selected>６</OPTION>
+							<OPTION value="7">７</OPTION>
+							<OPTION value="8">８</OPTION>
+							<OPTION value="9">９</OPTION>
+						</SELECT>
+					</td>
+				</tr>
+				<tr>
+					<td><p>縦列：</p></td>
+					<td>
+							<SELECT id="col_max"name="col_max">
+							<OPTION value="1">１</OPTION>
+							<OPTION value="2">２</OPTION>
+							<OPTION value="3">３</OPTION>
+							<OPTION value="4">４</OPTION>
+							<OPTION value="5">５</OPTION>
+							<OPTION value="6" selected>６</OPTION>
+							<OPTION value="7">７</OPTION>
+							<OPTION value="8">８</OPTION>
+							<OPTION value="9">９</OPTION>
+						</SELECT>
+					</td>
+				</tr>
+			</table>
+
+	</FIELDSET>
+
+	<input name= "group" type="hidden" value= "<?= $group ?>">
+	<input id="next"type="submit"value="次へ">
+</div>
+<div class="right_box">
+
+	<div class="user_list">
+	<table>
+<?php
+	$sql = "SELECT m_user.user_seq,m_user.user_name
+		from group_details,m_user
+			where group_details.group_seq = '$group'
+				and group_details.user_seq = m_user.user_seq";
+	$res = mysql_query($sql);
+	while($gyo = mysql_fetch_array($res))
+	{
+?>
+		<tr>
+			<td>
+				<input type="checkbox" name="user[]" value="<?= $gyo['user_seq']?>" checked="checked">
+			</td>
+			<td>
+				<p><?= $gyo['user_name']?></p>
+			</td>
+		</tr>
+<?php
+	}
+?>
+	</table>
+	</div>
+
+
+	</form>
+</div>
+	</body>
+</html>
