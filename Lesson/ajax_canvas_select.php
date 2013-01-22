64eb2d222e90bed23a5fb493808c8766f81a1ffe
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-01-22 14:44:13 +0900

    授業準備
    エラー

--- Lesson/ajax_canvas_select.php
+++ Lesson/ajax_canvas_select.php
@@ -17,7 +17,12 @@ $result_1 = array();
 for($i = 0;$i < $count;$i++){
 	$row = mysql_fetch_array($result);
 
-	$aaa = substr($row['div_url'],30);
+	//先頭文字によって添削する文字数を決める
+	if(substr($row['div_url'],0,4)=="http"){
+		$aaa = substr($row['div_url'],30);
+	}else{
+		$aaa = substr($row['div_url'],18);
+	}
 	$div = "url(../../balckboard/public/".$aaa;
 	$canvas = $row['canvas_url'];
 
