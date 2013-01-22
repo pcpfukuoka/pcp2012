136cbee6795f2f3f0c097a9dc074c9f470dc0e66
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-01-22 11:47:46 +0900

    授業準備
    新規作成

---
+++ Lesson/using_change.php
@@ -0,0 +1,17 @@
+<?php
+	//データベースの呼出
+	require_once("../lib/dbconect.php");
+	$dbcon = DbConnect();
+
+	//lesson_preparation.phpから送られてくるデータ
+	$date = $_POST['date'];
+	$subject_seq = $_POST['subject_seq'];
+
+
+	//準備中の背景画像のflagを使用中に変更
+	$using_change="UPDATE board SET end_flg = 'u' WHERE date = ".$date." AND class_seq = '15' AND subject_seq = ".$subject_seq.";";
+
+	$result=mysql_query($using_change);
+
+	//headerでlesson_preparationに移動させる
+?>
