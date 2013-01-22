b4e789d0282074bfd46d4dff784c2dbe1753c184
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-01-22 11:50:04 +0900

    エラー変更

--- Lesson/using_change.php
+++ Lesson/using_change.php
@@ -10,7 +10,7 @@
 
 	//準備中の背景画像のflagを使用中に変更
 	$using_change="UPDATE board SET end_flg = 'u' WHERE date = ".$date." AND class_seq = '15' AND subject_seq = ".$subject_seq.";";
-
+	echo using_change;
 	$result=mysql_query($using_change);
 
 	//headerでlesson_preparationに移動させる
