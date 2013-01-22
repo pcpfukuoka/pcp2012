6b78b4beb6643de04defd9acc74f5a1cbec6f162
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-01-22 12:06:20 +0900

    j授業準備
    エラー

--- Lesson/using_change.php
+++ Lesson/using_change.php
@@ -9,7 +9,7 @@
 
 
 	//準備中の背景画像のflagを使用中に変更
-	$using_change="UPDATE board SET end_flg = '1' WHERE date = ".$date." AND class_seq = '15' AND subject_seq = ".$subject_seq.";";
+	$using_change="UPDATE board SET end_flg = '1' WHERE date = '".$date."' AND class_seq = '15' AND subject_seq = '".$subject_seq."'AND end_flg='0';";
 	echo $using_change;
 	$result=mysql_query($using_change);
 
