a1eae4b764c847bd766fa5516aa4439e79e01480
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-01-24 14:29:19 +0900

    授業準備
    テスト

--- Lesson/chatroom.php
+++ Lesson/chatroom.php
@@ -11,6 +11,23 @@ setcookie("group_seq","15",$time,"/");
 
 setcookie("flg",true,$time,"/");
 
-header("Location:http://49.212.201.99:3000");
-
 ?>
+<html>
+<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
+
+		<form action="http://49.212.201.99:3000"method="post">
+			<input type="hidden" name="room" value="1">
+			<input type="submit" value="1">
+		</form>
+
+		<form action="http://49.212.201.99:3000"method="post">
+			<input type="hidden" name="room" value="2">
+			<input type="submit" value="2">
+		</form>
+
+</head>
+</html>
