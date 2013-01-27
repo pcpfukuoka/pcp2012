fb15e7dfd488022d4272b7c0ba5678d199fdb77c
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-01-27 19:31:43 +0900

    チャット
    テスト

--- Lesson/chatroom.php
+++ Lesson/chatroom.php
@@ -19,12 +19,12 @@ setcookie("flg",true,$time,"/");
 <body>
 		<form action="http://49.212.201.99:3000"method="post"enctype="multipart/form-data">
 			<input type="hidden" name="room" value="1" >
-			<input type="button" value="1" class="page_select"data-id="1">
+			<input type="button" value="1" class="page_select"data-id="3">
 		</form>
 
 		<form action="http://49.212.201.99:3000"method="post" enctype="multipart/form-data">
 			<input type="hidden" name="room" value="2">
-			<input type="button" value="2" class="page_select"data-id="2">
+			<input type="button" value="2" class="page_select"data-id="4">
 		</form>
 
 </body>
