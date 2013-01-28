4d189c2b3cdfe6fe53bafe9c90e2a97f2441491a
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-01-28 13:45:05 +0900

    授業準備
    header()関数の追加

--- Lesson/using_change.php
+++ Lesson/using_change.php
@@ -11,7 +11,6 @@
 
 	//準備中の背景画像のflagを使用中に変更
 	$using_change="UPDATE board SET end_flg = '1' WHERE date = '".$date."' AND class_seq = '".$group_seq."' AND subject_seq = '".$subject_seq."'AND end_flg='0';";
-	echo $using_change;
 	$result=mysql_query($using_change);
 
 	//headerでjoin_lesson.phpに移動させる
