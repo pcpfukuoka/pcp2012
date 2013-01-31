488b781c82c5b1d174a01b318b1bbdedf420ee7f
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-01-31 11:49:41 +0900

    アンケート
    デザイン

--- QuestionManager/answer_regist_view.php
+++ QuestionManager/answer_regist_view.php
@@ -14,19 +14,19 @@ $user_seq = $_SESSION['login_info[user]'];
 		<META http-equiv="Content-Style-Type" content="text/css">
 		<link rel="stylesheet" type="text/css" href="../css/back_ground.css" />
 		<link rel="stylesheet" type="text/css" href="../css/button.css" />
-		<link rel="stylesheet" type="text/css" href="../css/text_display.css" />	
+		<link rel="stylesheet" type="text/css" href="../css/text_display.css" />
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
 	</head>
-	<body>		
-		<?php 
+	<body>
+		<?php
 		require_once("../lib/dbconect.php");
 		$dbcon = DbConnect();
 		$sql = "SELECT question_title, question_description FROM question WHERE question_seq = $question_seq;";
 		$question_result = mysql_query($sql);
 		$question_row = mysql_fetch_array($question_result);
-		
-		
+
+
 		?>
 				<img class="bg" src="../images/blue-big.jpg" alt="" />
 		<div id="container">
@@ -35,18 +35,17 @@ $user_seq = $_SESSION['login_info[user]'];
 			</div>
 		<hr color="blue"><br><br>
 
-		<div align = "center">
-		
+
 		<form method ="post" action="answer_regist.php">
 		<input type="hidden" name="question_seq" value="<?= $question_seq ?>">
 		タイトル:<?= $question_row['question_title'] ?><br>
 		内容：<?= $question_row['question_description'] ?><br>
-		<?php 
+		<?php
 		//質問数を取得してその数ループで回答欄を作成
 		$sql = "SELECT question_details_seq, quesion_details_description,question_division.division_type  FROM question_details  LEFT JOIN question_division ON question_details.quesion_division = question_division.division_seq WHERE question_seq = $question_seq";
 		$details_result = mysql_query($sql);
 		$details_cnt = mysql_num_rows($details_result);
-		
+
 		//Details_seqを使って回答内容を取得してそれを元に回答欄を作成
 		for($i=0;$i<$details_cnt;$i++)
 		{
@@ -56,51 +55,50 @@ $user_seq = $_SESSION['login_info[user]'];
 			$awnser_resutl = mysql_query($sql);
 			$awnser_cnt = mysql_num_rows($awnser_resutl);?>
 			回答内容:<?= $details_row['quesion_details_description'] ?>
-<?php 
+<?php
 			for($j=0; $j < $awnser_cnt;$j++)
 			{
 				$awnser_row = mysql_fetch_array($awnser_resutl);
 				?>
-				<input type="<?= $details_row['division_type']?>" name="question_<?= $i + 1 ?>[]" value="<?= $awnser_row['question_awnser_list_seq'] ?>"><?= $awnser_row['awnser_name'] ?>				
-				<?php 
+				<input type="<?= $details_row['division_type']?>" name="question_<?= $i + 1 ?>[]" value="<?= $awnser_row['question_awnser_list_seq'] ?>"><?= $awnser_row['awnser_name'] ?>
+				<?php
 			}?>
 			その他：<input type="checkbox" class="etc_input" id="etc_input_<?= $i + 1 ?>" data-id="<?= $i + 1 ?>">
 				  <input type="text" name="etc_<?= $i + 1 ?>" disabled="true">
 			<input type="hidden" name="details_seq[]" value="<?= $details_row['question_details_seq']?>">
 			<br>
-	<?php 	
+	<?php
 		}
 		?>
 		<input class="button4"type="submit" value ="回答を送信">
-		</form>		
-				</div>
+		</form>
 		</div>
-		
+
 	</body>
-	
+
 			<script>
 		$(function() {
 			//検索結果から権限を追加するための処理
 			$(document).on('click', '.etc_input', function() {
 				//選択したli要素からdata-idを取得する(data-idはm_userのuser_seq)
-				
+
 		        var id = $(this).data('id');
 				var chk_str = 'etc_input_'+id;
 				var txt_str = 'etc_'+id;
 				if(document.getElementById(chk_str).checked)
 				{
 					$("*[name="+txt_str+"]").attr('disabled', false);
-								
+
 				}
 				else
 				{
 					$("*[name="+txt_str+"]").attr('disabled', true);
-								
+
 				}
 			});
 		});
 			</script>
-	
-	
-	
+
+
+
 </html>
