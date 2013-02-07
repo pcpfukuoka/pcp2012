ab0efcef854b6347ab8f08a2f200fa2de9d72f24
shinjinishihara <shinnji_classroom@yahoo.co.jp>
2013-02-07 16:10:31 +0900

    チェック
    更新

--- UserManager/change_view.php
+++ UserManager/change_view.php
@@ -35,41 +35,41 @@
 		$user_row = mysql_fetch_array($result_user);
 		?>
 
-		<form method ="post" action="change.php">
+		<form method ="post" action="userCheck.php">
 		<table>
 			<tr>
 				<td align="center">ユーザID:</td>
-				<td align="center"><input type="text" name="user_id" id="user_id" value="<?= $user_row['user_id'] ?>" disabled="true" Onblur="check('#user_id', 'ic,ac,tc', 0, 0)"></td>
+				<td align="center"><input type="text" name="user_id" id="user_id" value="<?= $user_row['user_id'] ?>" disabled="true"></td>
 			</tr>
 
 			<tr>
 				<td align="center">パスワード：</td>
-				<td align="center"><input type="text" name="pass" id="pass" value="<?= $user_row['pass'] ?>" disabled="true" Onblur="check('#pass', 'ic,ac,tc', 0, 0)"></td>
+				<td align="center"><input type="text" name="pass" id="pass" value="<?= $user_row['pass'] ?>" disabled="true"></td>
 			</tr>
 
 			<tr>
 				<td align="center">ユーザ名</td>
-				<td align="center"><input type="text" name="user_name" id="user_name" value="<?= $user_row['user_name'] ?>" disabled="true" Onblur="check('#user_name', 'ic,tc', 0, 0)"></td>
+				<td align="center"><input type="text" name="user_name" id="user_name" value="<?= $user_row['user_name'] ?>" disabled="true"></td>
 			</tr>
 
 			<tr>
 				<td align="center">ふりがな：</td>
-				<td align="center"><input type="text" name="user_kana" id="user_kana" value="<?= $user_row['user_kana'] ?>" disabled="true" Onblur="check('#user_kana', 'ic,fc,tc', 0, 0)"></td>
+				<td align="center"><input type="text" name="user_kana" id="user_kana" value="<?= $user_row['user_kana'] ?>" disabled="true"></td>
 			</tr>
 
 			<tr>
 				<td align="center">住所：</td>
-				<td align="center"><input type="text" name="user_address" id="user_address" value="<?= $user_row['user_address'] ?>" disabled="true" Onblur="check('#user_address', 'ic,tc', 0, 0)"></td>
+				<td align="center"><input type="text" name="user_address" id="user_address" value="<?= $user_row['user_address'] ?>" disabled="true"></td>
 			</tr>
 
 			<tr>
 				<td align="center">電話番号:</td>
-				<td align="center"><input type="text" name="user_tel" id="user_tel" value="<?= $user_row['user_tel'] ?>" disabled="true" Onblur="check('#user_tel', 'ic,nc,lc', 10, 10)"></td>
+				<td align="center"><input type="text" name="user_tel" id="user_tel" value="<?= $user_row['user_tel'] ?>" disabled="true"></td>
 			</tr>
 
 			<tr>
 				<td align="center">メールアドレス：</td>
-				<td align="center"><input type="text" name="user_email" id="user_email" value="<?= $user_row['user_email'] ?>" disabled="true" Onblur="check('#user_email', 'ic,mc', 0, 0)"></td>
+				<td align="center"><input type="text" name="user_email" id="user_email" value="<?= $user_row['user_email'] ?>" disabled="true"></td>
 			</tr>
 
 			<tr>
@@ -100,7 +100,7 @@
 		</tr>
 		<tr>
 			<td>学籍番号※学生のみ</td>
-			<td><input type="text" name="stuent_id" id="student_id" value="<?= $user_row['student_id'] ?>" disabled Onblur="check('#student_id', 'ic,nc,tc,lc', 6, 6)"></td>
+			<td><input type="text" name="student_id" id="student_id" value="<?= $user_row['student_id'] ?>" disabled></td>
 		</tr>
 	</table>
 
@@ -109,6 +109,7 @@
 		<tr>
 		<td>
 		<input type="hidden" value="<?= $user_row['user_seq'] ?>" name="user_seq">
+		<input type = "hidden" name = "regist_flg" value = "1">
 		<input class="button4" type="submit" value ="更新">
 		</form>
 		</td>
--- UserManager/userCheck.php
+++ UserManager/userCheck.php
@@ -1,14 +1,73 @@
 <?php
-$user_id = $_POST['user_id'];
-$pass = $_POST['pass'];
-$user_name = $_POST['user_name'];
-$user_kana = $_POST['user_kana'];
-$user_address = $_POST['user_address'];
-$user_tel = $_POST['user_tel'];
-$user_email = $_POST['user_email'];
-$autho_seq = $_POST['autho_seq'];
+session_start();
 
-require_once("../javascript/form_reference.js");
+//受け取ったデータをSESSIONに入れる
+$_SESSION['user_id'] = $_POST['user_id'];
+$_SESSION['pass'] = $_POST['pass'];
+$_SESSION['user_name'] = $_POST['user_name'];
+$_SESSION['user_kana'] = $_POST['user_kana'];
+$_SESSION['user_address'] = $_POST['user_address'];
+$_SESSION['user_tel'] = $_POST['user_tel'];
+$_SESSION['user_email'] = $_POST['user_email'];
+$_SESSION['autho_seq'] = $_POST['autho_seq'];
 
-$id_check = checkU($user_id, 'ic,pc,tc', 0, 0);
 ?>
+<html>
+	<head>
+	<script src="../javascript/form_reference.js"></script>
+	<script src="../javascript/jquery-1.8.2.min.js"></script>
+	</head>
+
+
+<?php
+//受け取ったデータのチェック
+$id_check = checkU($_POST['user_id'], 'ic,pc,tc', 0, 0);
+$pass_check = checkU($_POST['pass'], 'ic,pc,tc', 0, 0);
+$name_check = checkU($_POST['user_name'], 'ic,tc', 0, 0);
+$kana_check = checkU($_POST['user_kana'], 'ic,fc,tc', 0, 0);
+$address_check = checkU($_POST['user_address'], 'ic,tc', 0, 0);
+$tel_check = checkU($_POST['user_email'], 'ic,nc,lc', 10, 10);
+$mail_check = checkU($_POST['autho_seq'], 'ic,mc', 0, 0);
+
+//学生番号が入っていた場合のチェック
+if(isset($_POST['stuent_id']))
+{
+	$_SESSION['student_id'] = $_POST['student_id'];
+	$student_check = $checkU($_POST['student_id'], 'ic,nc,tc,lc', 6, 6);
+}
+
+//エラーメッセージがある場合、メッセージをSESSIONに入れ、前ページに戻る
+if($id_check || $pass_check || $name_check || $kana_check ||
+	$address_check || $tel_check || $mail_check || $student_check)
+{
+	$_SESSION['id_check'] = $id_check;
+	$_SESSION['pass_check'] = $pass_check;
+	$_SESSION['name_check'] = $name_check;
+	$_SESSION['kana_check'] = $kana_check;
+	$_SESSION['address_check'] = $address_check;
+	$_SESSION['tel_check'] = $tel_check;
+	$_SESSION['mail_check'] = $mail_check;
+	$_SESSION['student_check'] = $student_check;
+
+	if($_POST['regist_flg'] == 0)
+	{
+		header("Location: regist_view.php");
+	}
+	if($_POST['regist_flg'] == 1)
+	{
+		header("Location: change_view.php");
+	}
+}
+else
+{
+	if($_POST['regist_flg'] == 0)
+	{
+		header("Location: regist.php");
+	}
+	if($_POST['regist_flg'] == 1)
+	{
+		header("Location: change.php");
+	}
+}
+?>
+</html>
