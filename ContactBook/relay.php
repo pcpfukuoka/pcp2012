d433febbf96dc5fb74fe013a80171d4de236c1b0
mizutachika <o_hara1145007@mail.goo.ne.jp>
2013-02-08 16:04:14 +0900

    連絡帳
    グループ対応処理追加

--- ContactBook/relay.php
+++ ContactBook/relay.php
@@ -79,7 +79,7 @@
         if(isset($group_seq))
         {
         	//グループに所属するuser_seqの件数取り出し
-			$sql = "SELECT group_details.user_seq FROM group_details
+			$sql = "SELECT group_details.user_seq AS user_seq FROM group_details
 					LEFT JOIN m_group ON group_details.group_seq = m_group.group_seq
 					LEFT JOIN m_user ON group_details.user_seq = m_user.user_seq
         			WHERE group_details.group_seq = '$group_seq'";
@@ -169,21 +169,49 @@
     	print "<script language=javascript>leftreload();</script>";
     	print "<script language=javascript>jump('Preservation.html','right');</script>";
     }
+    //出来た
     ///////////////////////////
     //アップデート（Send.php）/
     ///////////////////////////
     else if(isset($_POST['send_update']))
     {
     	//送信完了ボタンの時の処理
+    	$group_seq = $_POST['group_seq'];
     	$contact_book_seq = $_POST['contact_book_seq'];
+    	$reception_user_seq = $_POST['reception_user_seq'];
     	$contents = $_POST['contents'];
     	$title = $_POST['title'];
     	$link_id = $_POST['link_id'];
 
-    	$sql = "UPDATE contact_book
-    			SET title = '$title', contents = '$contents', send_flg = 0, new_flg = 1, delete_flg = 1, send_date = now()
-    			WHERE contact_book_seq = '$contact_book_seq'; ";
-    	mysql_query($sql);
+    	//グループ宛ての送信
+    	if($group_seq >= 0)
+    	{
+    		//グループに所属するuser_seqの件数取り出し
+    		$sql = "SELECT group_details.user_seq AS user_seq FROM group_details
+    				LEFT JOIN m_group ON group_details.group_seq = m_group.group_seq
+    				LEFT JOIN m_user ON group_details.user_seq = m_user.user_seq
+    				WHERE group_details.group_seq = '$group_seq'";
+    		$result = mysql_query($sql);
+    		$count = mysql_num_rows($result);
+
+    		for($i = 1; $i <= $count; $i++)
+    		{
+	    		$row = mysql_fetch_array($result);
+	    		$group_send_user_seq = $row['user_seq'];
+
+				$sql = "INSERT INTO contact_book (title, contents, send_user_seq, reception_user_seq, link_contact_book_seq, send_date, new_flg, send_flg, group_seq)
+	    				VALUE ('$title', '$contents', '$user_seq', '$group_send_user_seq', 'link_id', now(), '1', '0', '$group_seq')";
+	    		mysql_query($sql);
+    		}
+    	}
+    	//個人宛ての送信
+    	else
+    	{
+    		$sql = "UPDATE contact_book
+    				SET title = '$title', contents = '$contents', send_flg = 0, new_flg = 1, delete_flg = 1, send_date = now()
+    				WHERE contact_book_seq = '$contact_book_seq'; ";
+    		mysql_query($sql);
+    	}
 
     	//データベースを閉じる
     	Dbdissconnect($dbcon);
@@ -197,7 +225,9 @@
     else if(isset($_POST['Re_preservation']))
     {
 		//保存ボタンの時の処理
+    	$group_seq = $_POST['group_seq'];
 		$contact_book_seq = $_POST['contact_book_seq'];
+		$reception_user_seq = $_POST['reception_user_seq'];
 		$contents = $_POST['contents'];
 		$title = $_POST['title'];
 		$link_id = $_POST['link_id'];
