cedbf439d59f0cd4a50b9abf0f1a1252994bde92
sakata <o_hara1045104@mail.goo.ne.jp>
2013-02-12 15:49:36 +0900

    ファイル読み込み
    作成

--- DataManager/data_input_for_file.php
+++ DataManager/data_input_for_file.php
@@ -10,13 +10,13 @@
 		<link rel="stylesheet" type="text/css" href="../css/text_display.css" />
 		<link rel="stylesheet" type="text/css" href="../css/table.css" />
 		<link rel="stylesheet" type="text/css" href="../css/table_search.css" />
-		
+
 		<script src="../javascript/jquery-1.8.2.min.js"></script>
 		<script src="../javascript/jquery-ui-1.8.24.custom.min.js"></script>
 		<script src="../javascript/form_reference.js"></script>
 	</head>
 	<body>
-		<img class="bg" src="../images/blue-big.jpg" alt="" />	
+		<img class="bg" src="../images/blue-big.jpg" alt="" />
 		<div id="container">
 				<div align = "center">
 			<font class="Cubicfont">データ取り込み</font>
@@ -25,12 +25,13 @@
 		<form action="db_data_insert.php" method="post" enctype="multipart/form-data">
 	     <select name="target">
 	     	<option value="1">ユーザーデータ</option>
-	     </select>
+	     	<option value="2">出席データ</option>
+	     	</select>
 	      ファイル：<br />
 	      <input type="file" name="upload_file" size="30" /><br />
 	      <input type="hidden" name="mode" value="upload" /><br />
 	      <br />
-	      
+
 	      <input type="submit" value="アップロード" />
   		</form>
 		</div>
--- DataManager/db_data_insert.php
+++ DataManager/db_data_insert.php
@@ -2,13 +2,14 @@
       //ライブラリ読み込み
       require_once './Classes/PHPExcel.php';
       require_once './Classes/PHPExcel/IOFactory.php';
-  
+      $target = $_POST['target'];
+
       if($_POST{'mode'} == "upload"){
           //Excel読み込み
           $filepath = $_FILES["upload_file"]["tmp_name"];
           $objReader = PHPExcel_IOFactory::createReader('Excel5');
           $xlsObject = $objReader->load($filepath);
-      
+
           // セルから値を取得
           	$xlsObject->setActiveSheetIndex(0);
           	$xlsSheet = $xlsObject->getActiveSheet();
@@ -29,36 +30,64 @@
           		$j++;
           	}
       }
+
+
+      //ここ以降に処理を書く
+
       //DB接続
       require_once("../lib/dbconect.php");
       $dbcon = DbConnect();
-      
-      
-      for($i = 0; $i < count($data[$sheetTitle]); $i++)
-      {
-      	
-      	$user_name = $data[$sheetTitle][$i][0];
-      	$user_kana = $data[$sheetTitle][$i][1];
-      	$user_address = $data[$sheetTitle][$i][2];
-      	$user_tel = $data[$sheetTitle][$i][3];
-      	$user_email = $data[$sheetTitle][$i][4];
-      	$user_id = $data[$sheetTitle][$i][5];
-      	$pass = $data[$sheetTitle][$i][6];
-      	$autho_seq = $data[$sheetTitle][$i][7];
-      	$sql = "INSERT INTO m_user VALUES (0, $user_name', '$user_kana', '$user_address', '$user_tel', '$user_email', '$user_id', '$pass', '$autho_seq','0','0'); ";
-      	echo $sql;
-      	echo "<br>";
-      	mysql_query($sql);
-      	
-      	if($data[$sheetTitle][$i][8] != "0")
-      	{
-      		$user_seq = mysql_insert_id($dbcon);
-      		$student_id = $data[$sheetTitle][$i][8];
-      		$sql = "INSERT INTO m_student VALUES (0,'$user_seq','$student_id');";
-      		mysql_query($sql);
-      		echo $sql;
-      		echo "<br>";
-      		
-      	}
-      }
+
+	if($target == 1)
+	{
+		for($i = 0; $i < count($data[$sheetTitle]); $i++)
+		{
+
+			$user_name = $data[$sheetTitle][$i][0];
+			$user_kana = $data[$sheetTitle][$i][1];
+			$user_address = $data[$sheetTitle][$i][2];
+			$user_tel = $data[$sheetTitle][$i][3];
+			$user_email = $data[$sheetTitle][$i][4];
+			$user_id = $data[$sheetTitle][$i][5];
+			$pass = $data[$sheetTitle][$i][6];
+			$autho_seq = $data[$sheetTitle][$i][7];
+			$sql = "INSERT INTO m_user VALUES ('0', '$user_name', '$user_kana', '$user_address', '$user_tel', '$user_email', '$user_id', '$pass', '$autho_seq','0','0'); ";
+			echo $sql;
+			echo "<br>";
+			mysql_query($sql);
+
+			if($data[$sheetTitle][$i][8] != "0")
+			{
+			$user_seq = mysql_insert_id($dbcon);
+			$student_id = $data[$sheetTitle][$i][8];
+			$sql = "INSERT INTO m_student VALUES (0,'$user_seq','$student_id');";
+			mysql_query($sql);
+			echo $sql;
+			echo "<br>";
+
+			}
+		}
+	}else if($target == 2)
+	{
+		for($i = 0; $i < count($data[$sheetTitle]); $i++)
+		{
+
+		$group_seq = $data[$sheetTitle][$i][0];
+		$user_seq = $data[$sheetTitle][$i][1];
+		$date = $data[$sheetTitle][$i][2];
+		$Attendance_seq = $data[$sheetTitle][$i][3];
+		$Absence_seq = $data[$sheetTitle][$i][4];
+		$Leaving_seq = $data[$sheetTitle][$i][5];
+		$Letaness_seq= $data[$sheetTitle][$i][6];
+		$Absence_due_to_mourning_seq = $data[$sheetTitle][$i][7];
+		$sql = "INSERT INTO attendance VALUES ('0','$group_seq', '$user_seq', '$date', '$Attendance_seq', '$Absence_seq', '$Leaving_seq', '$Letaness_seq', '$Absence_due_to_mourning_seq','0','0'); ";
+		echo $sql;
+		echo "<br>";
+		mysql_query($sql);
+
+
+		}
+
+	}
+
       ?>
