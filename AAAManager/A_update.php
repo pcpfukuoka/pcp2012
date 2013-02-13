d6a0782fa25b82072a23650bf54e4a929a29a788
sakata <o_hara1045104@mail.goo.ne.jp>
2013-02-13 11:57:36 +0900

    kkk
    iiiii

--- AAAManager/A_update.php
+++ AAAManager/A_update.php
@@ -5,9 +5,10 @@
 	//データベースの呼出
 	require_once("../lib/dbconect.php");
 	$dbcon = DbConnect();
-
+	echo "aaaa";
 	while (isset($_POST['seq'][$i]))
 	{
+		echo "bbbb";
 		$seq = $_POST['seq'][$i];
 
 		$str = "date" . $i;
@@ -40,7 +41,7 @@
 		}
 
 		$sql = $sql . " WHERE Attendance_seq='$seq'";
-		//echo "<br>" . $sql;
+		echo "<br>" . $sql;
 		mysql_query($sql);
 
 		$i++;
